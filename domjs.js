404b9c3bdac67658e8aa08db394721954a9af8f6
Boanerget <antonyprogramacion96@gmail.com>
2017-04-25 11:27:16 -0400

    mov Agrego metodo _searchClass

--- domjs.js
+++ domjs.js
@@ -39,5 +39,26 @@ const domjs = {
 			let element = document.getElementById(idElement);
 			element.parentNode.removeChild(element);
 		}
-	}
+	},
+	//Metodos para ver los atributos de un nodo
+	
+	//Metodo ver si un nodo tiene una clase o no
+	_searchClass : function(idElement, classElement){
+		
+		if(idElement && classElement == undefined){
+			alert('Algún parametro no ha sido proporcionado al metodo _searchClass.');
+		}
+		else if(typeof(idElement) && typeof(classElement) == 'string'){
+			let element = document.getElementById(idElement); 
+			let exists = element.classList.contains(classElement);
+
+			if(exists == true){
+				alert(`El nodo si tiene una clase llamada = '${classElement}'`);
+			}
+			else{
+				alert(`El nodo no tiene una clase llamada = ${classElement}`);
+			}
+		}
+
+	},
 }
